8bbc94a0cb322fd661dbd90fdc22408c0eeb3e03
Rahul kumar <rahulkumar96369@gmail.com>
2019-10-10 15:37:45 +0530

    first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,20 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        for (int i=2;i<=4;i++){
+            for (int j=i;j>=1;j--){
+
+                System.out.print(j%2);
+            }
+            System.out.println();
+        }
+
+
+    }
+        }
---
+++ src/com/company/abc.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class abc
+{
+public static void main(String []args){
+
+    for (int i=1;i<=2;i++){
+     for (int j=i; j<=4;j++)
+
+         System.out.print(j);
+
+    }
+    System.out.println();
+}
+}
